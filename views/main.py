6dc7e420fd1a470e61b6cd9525465c873d00d2ee
JoseABermudez57 <jose.apep570@gmail.com>
2024-02-16 02:07:49 -0600

    [ADD] more drawing options added

--- views/main.py
+++ views/main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import tkinter as tk
 from pathlib import Path
-from tkinter import colorchooser, filedialog, Tk, Canvas, PhotoImage, Button
+from tkinter import colorchooser, filedialog, messagebox, Tk, Canvas, PhotoImage, Button, Entry
 from PIL import Image, ImageTk
 
 
@@ -17,10 +17,11 @@ window = Tk()
 window_title = "PAINT UP"
 window.title(window_title)
 actions = True
-canvas_width = 595
-canvas_height = 419
+canvas_width = 894
+canvas_height = 562
+image_fill_state = False
 
-window.geometry("600x500")
+window.geometry("899x643")
 window.configure(bg = "#FFFFFF")
 
 img = np.ones((canvas_height, canvas_width, 3), dtype = np.uint8)*255
@@ -41,18 +42,18 @@ def create_widgets():
     canvas = Canvas(
         window,
         bg = "#FFFFFF",
-        height = 500,
-        width = 600,
+        height = 643,
+        width = 899,
         bd = 0,
         highlightthickness = 0,
         relief = "ridge"
     )
-    
+
     canvas.place(x = 0, y = 0)
-    image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-    
+    image_image_1 = PhotoImage(
+        file=relative_to_assets("image_1.png"))
     image_1 = canvas.create_image(
-        300.0,
+        449.0,
         38.0,
         image=image_image_1
     )
@@ -67,8 +68,8 @@ def create_widgets():
         relief="flat"
     )
     button_1.place(
-        x=112.0,
-        y=13.07452392578125,
+        x=83.0,
+        y=13.0,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -83,8 +84,8 @@ def create_widgets():
         relief="flat"
     )
     button_2.place(
-        x=46.0,
-        y=14.0,
+        x=17.0,
+        y=13.92547607421875,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -99,8 +100,8 @@ def create_widgets():
         relief="flat"
     )
     button_3.place(
-        x=177.80796813964844,
-        y=13.07452392578125,
+        x=148.80796813964844,
+        y=13.0,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -115,8 +116,8 @@ def create_widgets():
         relief="flat"
     )
     button_4.place(
-        x=243.61593627929688,
-        y=13.07452392578125,
+        x=214.61593627929688,
+        y=13.0,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -131,8 +132,8 @@ def create_widgets():
         relief="flat"
     )
     button_5.place(
-        x=309.4239196777344,
-        y=13.07452392578125,
+        x=280.4239196777344,
+        y=13.0,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -147,8 +148,8 @@ def create_widgets():
         relief="flat"
     )
     button_6.place(
-        x=375.23187255859375,
-        y=13.07452392578125,
+        x=346.23187255859375,
+        y=13.0,
         width=48.15217208862305,
         height=48.15217208862305
     )
@@ -163,20 +164,114 @@ def create_widgets():
         relief="flat"
     )
     button_7.place(
-        x=441.03985595703125,
-        y=13.07452392578125,
+        x=412.0,
+        y=13.92547607421875,
         width=48.15217208862305,
         height=48.15217208862305
     )
 
+    button_image_8 = PhotoImage(
+        file=relative_to_assets("button_8.png"))
+    button_8 = Button(
+        image=button_image_8,
+        borderwidth=0,
+        highlightthickness=0,
+        command=save_draw,
+        relief="flat"
+    )
+    button_8.place(
+        x=478.0,
+        y=13.92547607421875,
+        width=48.15217208862305,
+        height=48.15217208862305
+    )
+
+    button_image_10 = PhotoImage(file=relative_to_assets("button_10.png"))
+
+    def fill_figures():
+        global image_fill_state
+        
+        if not image_fill_state:
+            button_9.configure(image=button_image_10)
+            image_fill_state = True
+            brush[1] = -1
+        else:
+            button_9.configure(image=button_image_9)
+            image_fill_state = False
+            brush[1] = 3
+
+
+    button_image_9 = PhotoImage(
+        file=relative_to_assets("button_9.png"))
+    button_9 = Button(
+        image=button_image_9,
+        borderwidth=0,
+        highlightthickness=0,
+        command=fill_figures,
+        relief="flat"
+    )
+    button_9.place(
+        x=544.0,
+        y=14.0,
+        width=48.15217208862305,
+        height=48.15217208862305
+    )
+
+
     image_image_2 = PhotoImage(
         file=relative_to_assets("image_2.png"))
     image_2 = canvas.create_image(
-        531.0,
+        860.0,
         37.0,
         image=image_image_2
     )
 
+    image_image_3 = PhotoImage(
+        file=relative_to_assets("image_3.png"))
+    image_3 = canvas.create_image(
+        761.0,
+        38.0,
+        image=image_image_3
+    )
+
+    image_image_4 = PhotoImage(
+        file=relative_to_assets("image_4.png"))
+    image_4 = canvas.create_image(
+        676.0,
+        44.0,
+        image=image_image_4
+    )
+
+    def save_entry_value(event):
+        if entry_1.get():
+            try:
+                brush[1] = int(entry_1.get())
+            except:
+                messagebox.showinfo("Alerta", "El valor de tamaño de pincel no es un valor valido")
+
+
+    entry_image_1 = PhotoImage(
+        file=relative_to_assets("entry_1.png"))
+    entry_bg_1 = canvas.create_image(
+        761.0,
+        38.0,
+        image=entry_image_1
+    )
+    entry_1 = Entry(
+        bd=0,
+        bg="#FFFFFF",
+        fg="#000716",
+        highlightthickness=0,
+        font=('Comic Sans MS', 12, 'normal')
+    )
+    entry_1.place(
+        x=735.0,
+        y=24.0,
+        width=52.0,
+        height=26.0
+    )
+
+    entry_1.bind("<Return>", save_entry_value)
 
     img_pil = Image.fromarray(img)
     img_tk = ImageTk.PhotoImage(img_pil)
@@ -190,7 +285,7 @@ def create_widgets():
     
 
 def update_image():
-    global img, temp_img, img_tk, paint_img
+    global img, temp_img, img_tk, paint_img, img_pil
     
     img_pil = Image.fromarray(temp_img)
     img_tk = ImageTk.PhotoImage(img_pil)
@@ -222,23 +317,28 @@ def draw(event):
         temp_img = img.copy()
         x2, y2, = x, y
         radious = get_radious(x1, y1, x2, y2)
-        cv2.circle(temp_img, (x1, y1), int(radious), color, 2, lineType=cv2.LINE_AA)
+        cv2.circle(temp_img, (x1, y1), int(radious), color, brush[1], lineType=cv2.LINE_AA)
         update_image()
     if brush[0] == 'rectangle':
         temp_img = img.copy()
-        cv2.rectangle(temp_img, (x1, y1), (x, y), color, 2, lineType=cv2.LINE_AA)
+        cv2.rectangle(temp_img, (x1, y1), (x, y), color, brush[1], lineType=cv2.LINE_AA)
         update_image()
     if brush[0] == 'pencil':
+        thick = 3 if brush[1] < 0 else brush[1]
         temp_img = img.copy()
-        cv2.line(img, (ix, iy), (x, y), color, thickness=2, lineType=cv2.LINE_AA)
+        cv2.line(img, (ix, iy), (x, y), color, thickness=thick, lineType=cv2.LINE_AA)
         ix, iy = x, y
         update_image()
     if brush[0] == 'line':
+        thick = 3 if brush[1] < 0 else brush[1]
         temp_img = img.copy()
-        cv2.line(temp_img, (x1, y1), (x, y), color, thickness=2, lineType=cv2.LINE_AA)
+        cv2.line(temp_img, (x1, y1), (x, y), color, thickness=thick, lineType=cv2.LINE_AA)
         update_image()
     if brush[0] == 'eraser':
-        cv2.circle(temp_img, (x, y), 20, (255, 255, 255), -1, lineType=cv2.LINE_AA)
+        thick = 3 if brush[1] < 0 else brush[1]
+        temp_img = img.copy()
+        cv2.line(img, (ix, iy), (x, y), (255, 255, 255), thickness=thick, lineType=cv2.LINE_AA)
+        ix, iy = x, y
         update_image()
 
 
@@ -281,8 +381,18 @@ def change_to_eraser():
 
 def delete_draw():
     global temp_img
-    temp_img = 255*np.ones((600, 800, 3), dtype = np.uint8)
+    temp_img = 255*np.ones((canvas_height, canvas_width, 3), dtype = np.uint8)
     update_image()
+
+
+def save_draw():
+    global img_pil
+
+    file_name = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("Archivos PNG", "*.png")])
+    
+    if file_name:
+        img_pil.save(file_name)
+        print("Imagen guardada exitosamente en:", file_name)
 
 
 create_widgets()
